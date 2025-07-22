969e8ad0df4bc28ba8d0dabd011ce40da338f098
Felix Boerner <ich@felix-boerner.de>
2025-07-22 10:40:50 +0200

    feat: do more hardening

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,7 +3,32 @@
 import pytest
 
 
-@pytest.mark.parametrize("config", [("LogLevel INFO"), ("AllowGroups operator")])
+@pytest.mark.parametrize(
+    "config",
+    [
+        # https://complianceascode.github.io/content-pages/guides/ssg-debian12-guide-cis_level1_server.html
+        ("LogLevel INFO"),
+        ("LoginGraceTime 60"),
+        ("PermitRootLogin no"),
+        ("MaxAuthTries 4"),
+        ("MaxSessions 10"),
+        ("HostbasedAuthentication no"),
+        ("IgnoreRhosts yes"),
+        ("PermitEmptyPasswords no"),
+        ("UsePAM yes"),
+        ("X11Forwarding no"),
+        ("PermitUserEnvironment no"),
+        ("ClientAliveInterval 300"),
+        ("ClientAliveCountMax 0"),
+        ("MaxStartups 10:30:60"),
+        ("AllowGroups operator"),
+        # https://complianceascode.github.io/content-pages/guides/ssg-debian12-guide-cis_level2_server.html
+        ("GSSAPIAuthentication no"),
+        # Public key authentication only
+        ("PubkeyAuthentication yes"),
+        ("PasswordAuthentication no"),
+    ],
+)
 def test_sshd_config(host, config):
     """Check sshd config file"""
     f = host.file("/etc/ssh/sshd_config")
